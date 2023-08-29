ec78bd8cdbacbb580f0df93d6d147e8255eb70d5
NatalyBMota <interactivedesign@yahoo.com>
2023-08-29 15:22:53 -0500

    Added the comment 'It is possible that this remainingAttempts variable should be reset to 5, instead of to 0' to line 78 of index.js.

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function setup() {
   console.log(`target number: ${targetNumber}`);
 
   // Reset number of attempts
-  remainingAttempts = 0;
+  remainingAttempts = 0; /* It is possible that this remaining attempts should be reset to 5, and not to 0. */
 
   // Enable the input and submit button 
   submitButton.disabled = false; //fixed the spelling of disabled
