c7e2e32c0a8f1fa38053cb464ffc3c7b4bbd3b06
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-08-29 20:43:38 -0500

    Changed the code to make it so users cannot simply submit a number lower than 1 or higher than 99, nor something that is not a number, without getting an error message displayed in the browser window.

--- index.js
+++ index.js
@@ -7,11 +7,30 @@ const maxGuessesMessage = document.getElementById('max-guesses');
 //const numberOfGuessesMessage = document.getElementById('num-of-guesses');
 const numberOfGuessesMessage = document.createElement('p');
 numberOfGuessesMessage.className = 'message';
+const outOfRangeLowMessage = document.createElement('p');
+outOfRangeLowMessage.className = 'message';
+outOfRangeLowMessage.innerHTML = 'You guessed a number below 1. <br>This did not count as a turn. Please try again. <br>Only numbers between 1 and 99 are allowed guesses.';
+outOfRangeLowMessage.style.textAlign = 'center';
+outOfRangeLowMessage.style.color = 'red';
+const outOfRangeHighMessage = document.createElement('p');
+outOfRangeHighMessage.className = 'message';
+outOfRangeHighMessage.innerHTML = 'You guessed above 99. <br>This did not count as a turn. Please try again. <br>Only numbers between 1 and 99 are allowed guesses.';
+outOfRangeHighMessage.style.textAlign = 'center';
+outOfRangeHighMessage.style.color = 'red';
+const notANumberMessage = document.createElement('p');
+notANumberMessage.className = 'message';
+notANumberMessage.innerHTML = 'You tried submitting a guess that is not a number by <br>pressing the submit button without any value in the input field. <br>This is not a valid input. Please submit a number. <br>Only numbers between 1 and 99 are allowed guesses.';
+notANumberMessage.style.textAlign = 'center';
+notANumberMessage.style.color = 'red';
 const messages = document.getElementsByClassName('message');
 //console.log(`${messages[4]}`);
 
 /* The code below find the div that stores the paragraphs with the class of message, and appends the new paragraph, which is stored in the variable numberOfGuessesMessage, to it. */
-tooHighMessage.parentNode.appendChild(numberOfGuessesMessage);
+const divWithParagraphs = tooHighMessage.parentNode;
+divWithParagraphs.appendChild(numberOfGuessesMessage);
+divWithParagraphs.appendChild(outOfRangeLowMessage);
+divWithParagraphs.appendChild(outOfRangeHighMessage);
+divWithParagraphs.appendChild(notANumberMessage);
 const correctMessage = document.getElementById('correct');
 
 let targetNumber;
@@ -30,42 +49,51 @@ function getRandomNumber(min, max) {
 
 function checkGuess() {
   // Get value from guess input element
-  const guess = parseInt(guessInput.value, 10);
-  attempts = attempts + 1;
-
   hideAllMessages();
+  const guess = parseInt(guessInput.value, 10);
+  if ((guess >= 1) && (guess <= 99) && (guess !== 'NaN')) {
+    attempts = attempts + 1;
 
-  if (guess === targetNumber) {
-    numberOfGuessesMessage.style.display = '';
-    numberOfGuessesMessage.innerHTML = `You made ${attempts} guesses`;
-
-    correctMessage.style.display = '';
-
-    submitButton.disabled = true;
-    guessInput.disabled = true;
-  }
-
-  if (guess !== targetNumber) {
-    if (guess < targetNumber) {
-      tooLowMessage.style.display = '';
-    } else {
-      tooHighMessage.style.display = '';
+    if (guess === targetNumber) {
+      numberOfGuessesMessage.style.display = '';
+      numberOfGuessesMessage.innerHTML = `You made ${attempts} guesses`;
+  
+      correctMessage.style.display = '';
+  
+      submitButton.disabled = true;
+      guessInput.disabled = true;
     }
 
-    let remainingAttempts = maxNumberOfAttempts - attempts;
+    if (guess !== targetNumber) {
+      if (guess < targetNumber) {
+        tooLowMessage.style.display = '';
+      } else {
+        tooHighMessage.style.display = '';
+      }
+  
+      let remainingAttempts = maxNumberOfAttempts - attempts;
+  
+      numberOfGuessesMessage.style.display = '';
+      numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
+    }
 
-    numberOfGuessesMessage.style.display = '';
-    numberOfGuessesMessage.innerHTML = `You guessed ${guess}. <br> ${remainingAttempts} guesses remaining`;
+    if (attempts === maxNumberOfAttempts) {
+      submitButton.disabled = true;
+      guessInput.disabled = true;
+    }
+  
+    guessInput.value = '';
+  
+    resetButton.style.display = '';
+  } else if (guess < 1) {
+    outOfRangeLowMessage.style.display = '';
+  } else if (guess > 99) {
+    outOfRangeHighMessage.style.display = '';
+  } else { 
+    /* This condition will run if guess === 'NaN', such as if the user presses the submit button without first either typing in or selecting a number. */
+    notANumberMessage.style.display = '';
   }
-
-  if (attempts === maxNumberOfAttempts) {
-    submitButton.disabled = true;
-    guessInput.disabled = true;
-  }
-
-  guessInput.value = '';
-
-  resetButton.style.display = '';
+  
 }
 
 function hideAllMessages() {
